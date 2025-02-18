b7d9d21854fd9a250eb697a3d92994312cc6e2ec
Mori <kittiwatpoonnart@gmail.com>
2025-02-18 19:21:07 +0700

    add lab12

---
+++ Lab12/Main.java
@@ -0,0 +1,9 @@
+import Lib.FormLab12_1;
+import Lib.FormLab12_2;
+
+public class Main {
+    public static void main(String[] args) {
+        new FormLab12_1();
+        new FormLab12_2();
+    }
+}
