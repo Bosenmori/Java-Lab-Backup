688d0d199340e02b975f8a1329093611bd588ccd
Mori <kittiwatpoonnart@gmail.com>
2025-03-04 18:52:34 +0700

    Add Lab14

--- Lab14/Main1.java
+++ Lab14/Main1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main1 {
+public class Main2 {
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(System.in);
